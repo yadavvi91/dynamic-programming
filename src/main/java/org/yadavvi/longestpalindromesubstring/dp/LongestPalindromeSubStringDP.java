000f100f4f1c79dc05d51d46d44a6465a018779e
Vishal Yadav <yadavvi91@gmail.com>
2017-04-11 16:20:55 +0530

    Does not work for the string ABACDFGDCABA.

--- src/main/java/org/yadavvi/longestpalindromesubstring/dp/LongestPalindromeSubStringDP.java
+++ src/main/java/org/yadavvi/longestpalindromesubstring/dp/LongestPalindromeSubStringDP.java
@@ -7,53 +7,47 @@ public class LongestPalindromeSubStringDP {
 
     public String longestPalindrome(String s) {
         String string = s;
-        int[] palindrome = new int[string.length()];
-        int maxlength = 1;
-        int maxlengthAt = 0;
+        int[][] palindrome = new int[string.length()][string.length()];
+        int maxLength = 0;
+        int maxI = 0, maxJ = 0;
 
-        for (int i = 0; i < string.length(); i++) {
-            palindrome[i] = 1;
-        }
+        // This is done by (i == j)
+        /*for (int i = 0; i < string.length(); i++) {
+            palindrome[i][i] = 1;
+        }*/
 
-        for (int k = 0; k < string.length(); k++) {
-            int iStart, jStart;
-            if (string.length() - 1 - k > k) {
-                iStart = 0;
-                jStart = k + iStart;
-            } else {
-                jStart = string.length() - 1;
-                iStart = k - jStart;
-            }
-            for (int j = jStart, i = iStart;
-                 ((j >= k && i < k) || (j > k && i <= k)) && i < j && i >= 0 && j <= string.length() - 1;
-                 i++, j--) {
-                if (string.charAt(i) == string.charAt(j)) {
-                    palindrome[k] += 2;
+        for (int j = 0; j < string.length(); j++) {
+            for (int i = 0; i < string.length(); i++) {
+                if (i == j) {
+                    palindrome[i][j] = 1;
+                    if (maxLength < palindrome[i][j]) {
+                        maxLength = 1;
+                        maxI = i;
+                        maxJ = j;
+                    }
+                } else {
+                    if (i >= j || j <= 0) continue;
+                    if (string.charAt(i + 1) == string.charAt(j - 1)) {
+                        if (i + 1 == j - 1) palindrome[i][j] = palindrome[i + 1][j - 1] + 1;
+                        else palindrome[i][j] = palindrome[i + 1][j - 1] + 2;
+                        if (maxLength < palindrome[i][j]) {
+                            maxLength = palindrome[i][j];
+                            maxI = i;
+                            maxJ = j;
+                        }
+                    } else {
+                        palindrome[i][j] = 1;
+                        if (maxLength < palindrome[i][j]) {
+                            maxLength = palindrome[i][j];
+                            maxI = i;
+                            maxJ = j;
+                        }
+                    }
                 }
             }
         }
-        for (int i = 0; i < string.length(); i++) {
-            if (palindrome[i] > maxlength) {
-                maxlength = palindrome[i];
-                maxlengthAt = i;
-            }
-        }
-
-        return getLongestPalindrome(string, maxlength, maxlengthAt);
-    }
-
-    private String getLongestPalindrome(String string, int maxlength, int maxlengthAt) {
-        int i = maxlengthAt - 1, j = maxlengthAt + 1;
-
-        for (; i > 0 && j < string.length() - 1 && j - i <= maxlength; i--, j++) {
-
-        }
-
-        if (maxlength == (j - i)) {
-            return string.substring(i, j);
-        } else {
-            return string.substring(i + 1, j);
-        }
+        // Since maxJ is inclusive
+        return string.substring(maxI, maxJ + 1);
     }
 
 }
