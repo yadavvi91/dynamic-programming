671af8f296830af2916121ec806d6d425099fb86
Vishal Yadav <yadavvi91@gmail.com>
2017-04-11 13:15:47 +0530

    Longest palindrome substring without Dynamic Programming.

---
+++ src/main/java/org/yadavvi/longestpalindromesubstring/nondp/LongestPalindromeSubStringNonDP.java
@@ -0,0 +1,31 @@
+package org.yadavvi.longestpalindromesubstring.nondp;
+
+/**
+ * Created by vishal on 11/4/17.
+ */
+public class LongestPalindromeSubStringNonDP {
+
+    public String longestPalindrome(String s) {
+        int start = 0, end = 0;
+        for (int i = 0; i < s.length(); i++) {
+            int len1 = expandAroundCenter(s, i, i);
+            int len2 = expandAroundCenter(s, i, i + 1);
+            int len = Math.max(len1, len2);
+            if (len > end - start) {
+                start = i - (len - 1) / 2;
+                end = i + len / 2;
+                System.out.println(s.substring(start, end + 1));
+            }
+        }
+        return s.substring(start, end + 1);
+    }
+
+    private int expandAroundCenter(String s, int left, int right) {
+        int L = left, R = right;
+        while (L >= 0 && R < s.length() && s.charAt(L) == s.charAt(R)) {
+            L--;
+            R++;
+        }
+        return R - L - 1;
+    }
+}
---
+++ src/test/java/org/yadavvi/longestpalindromesubstring/LongestPalindromeSubStringTest.java
@@ -0,0 +1,59 @@
+package org.yadavvi.longestpalindromesubstring;
+
+import junitparams.JUnitParamsRunner;
+import junitparams.Parameters;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.yadavvi.longestpalindromesubstring.dp.LongestPalindromeSubStringDP;
+import org.yadavvi.longestpalindromesubstring.nondp.LongestPalindromeSubStringNonDP;
+import org.yadavvi.longestpalindromesubstring.reversed.LongestPalindromeSubStringReversedString;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.junit.MatcherAssert.assertThat;
+
+/**
+ * Created by vishal on 11/4/17.
+ */
+@RunWith(JUnitParamsRunner.class)
+public class LongestPalindromeSubStringTest {
+
+    private LongestPalindromeSubStringNonDP longestPalindromeSubStringNonDP;
+    private LongestPalindromeSubStringDP longestPalindromeSubStringDP;
+    private LongestPalindromeSubStringReversedString longestPalindromeSubStringReversedString;
+
+    public Object[] stringAndPalindrome() {
+        return new Object[]{
+                new Object[]{"BABCBAB", "BABCBAB"},
+                new Object[]{"ABCBABA", "ABCBA"},
+                new Object[]{"ABBA", "ABBA"},
+                new Object[]{"ABACDFGDCABA", "ABA"}
+        };
+    }
+
+    @Before
+    public void setUp() throws Exception {
+        longestPalindromeSubStringNonDP = new LongestPalindromeSubStringNonDP();
+        longestPalindromeSubStringDP = new LongestPalindromeSubStringDP();
+        longestPalindromeSubStringReversedString = new LongestPalindromeSubStringReversedString();
+    }
+
+    @Test
+    @Parameters(method = "stringAndPalindrome")
+    public void longestPalindromeNonDP(String string, String palindrome) throws Exception {
+        assertThat(longestPalindromeSubStringNonDP.longestPalindrome(string), is(palindrome));
+    }
+
+    /*@Test
+    @Parameters(method = "stringAndPalindrome")
+    public void longestPalindromeDP(String string, String palindrome) throws Exception {
+        assertThat(longestPalindromeSubStringDP.longestPalindrome(string), is(palindrome));
+    }*/
+
+    /*@Test
+    @Parameters(method = "stringAndPalindrome")
+    public void longestPalindromeReversedString(String string, String palindrome) throws Exception {
+        assertThat(longestPalindromeSubStringReversedString.longestPalindrome(string), is(palindrome));
+    }*/
+
+}
